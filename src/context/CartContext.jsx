827b2a13f4b3233d9d230997a3d14e71d63433c8
ShreeyashShrestha <shreeyashofficialpc1@gmail.com>
2026-08-04 15:37:34 +0545

    Set up Context API

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+
+const CartContext = createContext();
+import { initialProducts } from "../data/product";
+
+export const CartProvider = (props) => {
+  const products = initialProducts;
+
+  return (
+    <CartContext.Provider value={{ products }}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,7 +1,9 @@
 import SearchFilter from '../components/SearchFilter'
 import CategoryFilter from '../components/CategoryFilter'
+import { useCart } from '../context/CartContext'
 
 const ProductList = () => {
+  console.log(useCart())
   return (
     <>
     <div className="container mx-auto px-4 md:px-8 pt-8"></div>
